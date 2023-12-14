b8eef38928ac901f39c8fb329805617c7bc17ebc
Horeb Barriga <74694397+Barr1ga@users.noreply.github.com>
2023-12-14 12:40:10 +0800

    fix workspace lambda expression

---
+++ KonnAPI/Helpers/MappingProfiles.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+using KonnAPI.Dto;
+using KonnAPI.Models;
+
+namespace KonnAPI.Helpers;
+
+public class MappingProfiles : Profile {
+    public MappingProfiles() {
+        CreateMap<Contact, ContactDto>();
+    }
+}
